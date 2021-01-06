1c5e18e2dcea1fb6f9897d334d1db6bb4116565f
Charles Kallick <67604746+charleskallick@users.noreply.github.com>
2021-01-06 14:20:19 -0700

    fixes and changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,14 +3,43 @@ import './App.css';
 import BusinessList from '../businessList/businesslist';
 import SearchBar from '../searchBar/searchbar';
 
-function App() {
-    return (
-        <div className="App">
-          <h1>ravenous</h1>
-          <SearchBar />
-          <BusinessList /> 
-        </div>
-    );
+
+const business = {
+    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+}
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
+class App extends React.Component {
+
+    searchYelp(term, location, sortBy) {
+        alert(`Searching Yelp with ${term}, ${location}, ${sortBy}.`);
+    };
+
+    render() {
+        return (
+          <div className="App">
+            <h1>ravenous</h1>
+            <SearchBar searchYelp={this.searchYelp} />
+            <BusinessList businesses={businesses} /> 
+          </div>
+        );
+    }
 }
 
 export default App;
