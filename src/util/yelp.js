124dac861551ccbe8d21dda6d5931ac0e36a3314
Charles Kallick <67604746+charleskallick@users.noreply.github.com>
2021-01-08 15:03:29 -0700

    search fixes

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -6,23 +6,22 @@ const Yelp = {
             headers: {
                 Authorization: `Bearer ${apiKey}`
             }
-        })
-        .then(jsonResponse => {
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
             if (jsonResponse.businesses) {
-                return jsonResponse.businesses.map(business => {
-                    return {
-                        id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        address: business.address,
-                        city: business.city,
-                        state: business.state,
-                        zipCode: business.zipCode,
-                        category: business.category,
-                        rating: business.rating,
-                        reviewCount: business.reviewCount
-                    };    
-                });
+                return jsonResponse.businesses.map(business => ({
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                }));
             }
         });
     }
